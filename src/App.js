9dfb65d87cba14a403c3d67316449e7450696ee8
129171 <levietduydng@gmail.com>
2024-01-10 17:00:59 +0700

    ex4part1

--- src/App.js
+++ src/App.js
@@ -3,20 +3,41 @@ import PeopleInfo from "./PeopleInfo"; // Make sure the path to your PeopleInfo 
 import Navbar from "./Navbar";
 import CourseList from "./CourseList";
 
+
+import "./App.css"; // Make sure to create this CSS file with the styles provided below
+
+
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <h1>Welcome to the People Info App</h1>
-      </header>
-      <main>
-        <PeopleInfo />
-        <CourseList />
-      </main>
+    <div className="hello-react">
+      <Navbar />
+
+      Hello <span className="react-text">React</span>
+      <h1>My Courses</h1>
+      <CourseList />
+      <PeopleInfo />
+      <div style={{ textAlign: "center" }}>
+    {" "}
+    {/* Center align the content */}
+    <img
+      
+      alt="React Logo"
+      style={{ width: "200px", marginTop: "50px" }}
+    />{" "}
+    {/* Image with styles */}
+    <p style={{ fontStyle: "italic" }}>
+      This is the React logo! <br />
+      (I don't know why it is here either)
+    </p>
+    <p>The library for web and native user interfaces</p>
+  </div>
     </div>
+    
   );
+  
 };
+
+
 
 
 export default App;
