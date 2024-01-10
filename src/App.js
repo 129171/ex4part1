34a0560041b9f8661976acb9458786b67edd7426
129171 <levietduydng@gmail.com>
2024-01-10 16:48:41 +0700

    ex4part1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import PeopleInfo from "./PeopleInfo"; // Make sure the path to your PeopleInfo component is correct
-
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <h1>Welcome to the People Info App</h1>
       </header>
       <main>
         <PeopleInfo />
+
       </main>
     </div>
   );
